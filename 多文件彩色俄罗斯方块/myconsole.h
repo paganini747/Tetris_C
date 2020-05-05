0dee58ccb9423f707b3853893e737bcc7a367dd1
GitHubYuanHao <49775631+GitHubYuanHao@users.noreply.github.com>
2020-05-05 17:11:04 +0800

    Add files via upload

---
+++ 多文件彩色俄罗斯方块/myconsole.h
@@ -0,0 +1,12 @@
+#ifndef MYCONSOLE
+#define MYCONSOLE
+
+long GetTime();
+void GotoXY(int x, int y);
+void HideCursor();
+int Color(int c);
+void PlayMusic();
+void RestartMusic();
+void PauseMusic();
+
+#endif
---
+++ 多文件彩色俄罗斯方块/mytetris.h
@@ -0,0 +1,18 @@
+#ifndef TETRIS
+#define TETRIS
+
+void GameOver();
+void Initialization();
+void MakeTetris();
+void PrintTetris();
+void CleanOld();
+void DrawNew();
+void Transform();
+void Judge_MoveAndTansform();
+void JustForDebugging();
+void PrintNextTetris();
+void CleanLine();
+void AutoDown();
+void KeyControl();
+
+#endif
